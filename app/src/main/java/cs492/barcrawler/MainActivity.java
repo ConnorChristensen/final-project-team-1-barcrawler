f0d91b1041c5242ef334e86bc256ae7b63d1becb
Connor Christensen <chriconn@oregonstate.edu>
2018-03-03 20:22:45 -0800

    connected the slider to the bar number
    When the user moves the slider it is reflected in the bar number text.

--- app/src/main/java/cs492/barcrawler/MainActivity.java
+++ app/src/main/java/cs492/barcrawler/MainActivity.java
@@ -2,12 +2,37 @@ package cs492.barcrawler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekBar;
+    private TextView mBarNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBarNumber = findViewById(R.id.bar_number);
+
+        mSeekBar = findViewById(R.id.bar_slider);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mBarNumber.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
