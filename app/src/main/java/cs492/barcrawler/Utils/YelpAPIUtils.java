3fc95c886aba0ec8f7d4380ef250e8031d25388e
WilliamBuffum <buffumw@oregonstate.edu>
2018-03-07 16:52:06 -0800

    Created YelpAPIUtils class and added basic static strings for querying

---
+++ app/src/main/java/cs492/barcrawler/Utils/YelpAPIUtils.java
@@ -0,0 +1,20 @@
+package cs492.barcrawler.Utils;
+
+/**
+ * Created by Billy on 3/7/18.
+ */
+
+public class YelpAPIUtils {
+    private static final String YELP_API_BASE_URL = "https://api.yelp.com/v3/businesses/search";
+    private static final String YELP_API_KEY = "5Sp7JRrDxKa4pEsh-JNADYa_Z2ZHRyvytSPcxN-Gi-0ySXfbTC4gI1Vr8FEblrFQoKDYgZENPw3U-PDS69F0aB_Pih4GACpYONmM2AthKeBWe8JLskKUnfRAooWgWnYx";
+    private static final String YELP_API_CLIENT_ID = "H3Fk8CujrwlfrT13Fh2P6w";
+    private static final String YELP_API_LATITUDE_PARAM = "latitude";
+    private static final String YELP_API_LONGITUDE_PARAM = "longitude";
+    private static final String YELP_API_RADIUS_PARAM = "radius";
+    private static final String YELP_API_CATEGORIES_PARAM = "categories";
+    private static final String YELP_API_PRICE_PARAM = "price";
+    private static final String YELP_API_SORT_BY_PARAM = "sort_by";
+    private static final String YELP_API_OPEN_NOW_PARAM = "open_now";
+
+
+}
