e1ee346d3e54f3c8bc367526ab6a6983eb4cf7ad
WilliamBuffum <buffumw@oregonstate.edu>
2018-03-07 17:15:36 -0800

    Created base class and added static final constants used in URL

--- app/src/main/java/cs492/barcrawler/Utils/YelpAPIUtils.java
+++ app/src/main/java/cs492/barcrawler/Utils/YelpAPIUtils.java
@@ -1,12 +1,15 @@
 package cs492.barcrawler.Utils;
 
+import com.yelp.clientlib.connection.YelpAPI;
+
+import java.io.Serializable;
+
 /**
  * Created by Billy on 3/7/18.
  */
 
 public class YelpAPIUtils {
     private static final String YELP_API_BASE_URL = "https://api.yelp.com/v3/businesses/search";
-    private static final String YELP_API_KEY = "5Sp7JRrDxKa4pEsh-JNADYa_Z2ZHRyvytSPcxN-Gi-0ySXfbTC4gI1Vr8FEblrFQoKDYgZENPw3U-PDS69F0aB_Pih4GACpYONmM2AthKeBWe8JLskKUnfRAooWgWnYx";
     private static final String YELP_API_CLIENT_ID = "H3Fk8CujrwlfrT13Fh2P6w";
     private static final String YELP_API_LATITUDE_PARAM = "latitude";
     private static final String YELP_API_LONGITUDE_PARAM = "longitude";
@@ -16,5 +19,9 @@ public class YelpAPIUtils {
     private static final String YELP_API_SORT_BY_PARAM = "sort_by";
     private static final String YELP_API_OPEN_NOW_PARAM = "open_now";
 
+    private static final String YELP_API_AUTH_HEADER_NAME = "Authorization"
+    private static final String YELP_API_AUTH_HEADER_VALUE = "BEARER 5Sp7JRrDxKa4pEsh-JNADYa_Z2ZHRyvytSPcxN-Gi-0ySXfbTC4gI1Vr8FEblrFQoKDYgZENPw3U-PDS69F0aB_Pih4GACpYONmM2AthKeBWe8JLskKUnfRAooWgWnYx";
 
+    public static class YelpItem implements Serializable {
+    }
 }
