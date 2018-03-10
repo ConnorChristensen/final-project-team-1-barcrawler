38f47478709cebd2955e3dea43e6d7889bdeb8cc
WilliamBuffum <buffumw@oregonstate.edu>
2018-03-10 10:17:50 -0800

    Created the serializable YelpItem static class and built a minimum URL builder

--- app/src/main/java/cs492/barcrawler/Utils/YelpAPIUtils.java
+++ app/src/main/java/cs492/barcrawler/Utils/YelpAPIUtils.java
@@ -1,8 +1,11 @@
 package cs492.barcrawler.Utils;
 
+import android.net.Uri;
+
 import com.yelp.clientlib.connection.YelpAPI;
 
 import java.io.Serializable;
+import java.net.URL;
 
 /**
  * Created by Billy on 3/7/18.
@@ -19,9 +22,34 @@ public class YelpAPIUtils {
     private static final String YELP_API_SORT_BY_PARAM = "sort_by";
     private static final String YELP_API_OPEN_NOW_PARAM = "open_now";
 
-    private static final String YELP_API_AUTH_HEADER_NAME = "Authorization"
-    private static final String YELP_API_AUTH_HEADER_VALUE = "BEARER 5Sp7JRrDxKa4pEsh-JNADYa_Z2ZHRyvytSPcxN-Gi-0ySXfbTC4gI1Vr8FEblrFQoKDYgZENPw3U-PDS69F0aB_Pih4GACpYONmM2AthKeBWe8JLskKUnfRAooWgWnYx";
+    public static final String YELP_API_AUTH_HEADER_NAME = "Authorization";
+    public static final String YELP_API_AUTH_HEADER_VALUE = "BEARER 5Sp7JRrDxKa4pEsh-JNADYa_Z2ZHRyvytSPcxN-Gi-0ySXfbTC4gI1Vr8FEblrFQoKDYgZENPw3U-PDS69F0aB_Pih4GACpYONmM2AthKeBWe8JLskKUnfRAooWgWnYx";
 
     public static class YelpItem implements Serializable {
+        public String barName;
+        public String description;
+        public URL imageURL;
+        public URL yelpURL;
+        public int rating;
+        public String price;
+        public String phoneNumber;
+        public String address;
+        public String city;
+        public String state;
+        public String zipcode;
+        public float distance;
+        public boolean isClosed;
     }
+
+    public static String buildYelpSearchURL(String latitude, String longitude) {
+        return Uri.parse(YELP_API_BASE_URL).buildUpon()
+                .appendQueryParameter(YELP_API_LATITUDE_PARAM, latitude)
+                .appendQueryParameter(YELP_API_LONGITUDE_PARAM, longitude)
+                .appendQueryParameter(YELP_API_CATEGORIES_PARAM, "bars,brewery,pub")
+                .appendQueryParameter(YELP_API_RADIUS_PARAM, "100")
+                .appendQueryParameter(YELP_API_OPEN_NOW_PARAM, "true")
+                .build()
+                .toString();
+    }
+
 }
