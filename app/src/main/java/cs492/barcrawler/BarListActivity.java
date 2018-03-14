dc99e776350a3ed440d2239137c7a3ffefeaf24e
Connor Christensen <chriconn@oregonstate.edu>
2018-03-13 18:21:29 -0700

    bound main go button to a bar list activity
    Created a bar list activity and created an intent to make the go button on the main activity open the bar list activity.

---
+++ app/src/main/java/cs492/barcrawler/BarListActivity.java
@@ -0,0 +1,13 @@
+package cs492.barcrawler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BarListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_list);
+    }
+}
--- app/src/main/java/cs492/barcrawler/MainActivity.java
+++ app/src/main/java/cs492/barcrawler/MainActivity.java
@@ -1,7 +1,10 @@
 package cs492.barcrawler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -9,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private SeekBar mSeekBar;
     private TextView mBarNumber;
+    private Button mGoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBarNumber = findViewById(R.id.bar_number);
+        mGoButton = findViewById(R.id.main_go_button);
+
+        mGoButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent barList = new Intent(view.getContext(), BarListActivity.class);
+                startActivity(barList);
+            }
+        });
 
         mSeekBar = findViewById(R.id.bar_slider);
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
