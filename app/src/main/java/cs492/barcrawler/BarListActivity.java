a677f8456ab53c50f2804cee01a21a7eb00ab50d
Connor Christensen <chriconn@oregonstate.edu>
2018-03-13 18:31:43 -0700

    added in loading bar and connection error message
    They are both set to invisible, but will show up later if the network request does not work.

--- app/src/main/java/cs492/barcrawler/BarListActivity.java
+++ app/src/main/java/cs492/barcrawler/BarListActivity.java
@@ -2,12 +2,22 @@ package cs492.barcrawler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class BarListActivity extends AppCompatActivity {
+
+    // loading elements
+    private ProgressBar mLoadingIndicator;
+    private TextView mLoadingErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_list);
+
+        // get our loading indicator and error message in the activity_bar_list.xml
+        mLoadingIndicator = findViewById(R.id.bar_list_loading_indicator);
+        mLoadingErrorMessage = findViewById(R.id.loading_error_message);
     }
 }
