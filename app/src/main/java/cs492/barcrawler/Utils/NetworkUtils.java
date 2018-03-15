19065e62cc467f54d7fb5c762b6545ee51bcedaa
WilliamBuffum <buffumw@oregonstate.edu>
2018-03-15 14:41:41 -0700

    Modified the way we add headers

--- app/src/main/java/cs492/barcrawler/Utils/NetworkUtils.java
+++ app/src/main/java/cs492/barcrawler/Utils/NetworkUtils.java
@@ -3,7 +3,9 @@ package cs492.barcrawler.Utils;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.Map;
 
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,16 +18,22 @@ public class NetworkUtils {
     private static final OkHttpClient mHTTPClient = new OkHttpClient();
 
     public static String doHTTPGet(String url, String headerName, String headerValue) throws IOException {
+
+//        Map<String, String> header = new Map<String, String>;
+//        header.put(headerName,headerValue);
+//        Headers headers = Headers.of(header);
+
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader(headerName, headerValue)
+                .header(headerName, headerValue)
                 .build();
 
-        Log.d("NETWORK UTIL: ", request.toString());
+        Log.d("NETWORK UTIL: ", request.headers().toString());
 
         Response response = mHTTPClient.newCall(request).execute();
 
         try {
+//            Log.d("RESPONSE: ", response.body().string());
             return response.body().string();
         } finally {
             response.close();
