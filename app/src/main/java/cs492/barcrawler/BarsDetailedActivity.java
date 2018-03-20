5aa08db6b57a691b0924233b46ff22860478f579
Connor Christensen <chriconn@oregonstate.edu>
2018-03-20 14:30:49 -0700

    removed unnecessary information
    The city, state, country and zip code are already displayed in the location section

--- app/src/main/java/cs492/barcrawler/BarsDetailedActivity.java
+++ app/src/main/java/cs492/barcrawler/BarsDetailedActivity.java
@@ -14,11 +14,6 @@ public class BarsDetailedActivity extends AppCompatActivity {
     public TextView mBarPrice;
     public TextView mBarPhoneNumber;
     public TextView mBarLocation;
-//    public TextView mBarAddress;
-//    public TextView mBarCity;
-//    public TextView mBarState;
-//    public TextView mBarCountry;
-//    public TextView mBarZipCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +23,6 @@ public class BarsDetailedActivity extends AppCompatActivity {
         mBarPrice = findViewById(R.id.tv_bar_price);
         mBarPhoneNumber = findViewById(R.id.tv_bar_phone_number);
         mBarLocation = findViewById(R.id.tv_bar_location);
-//        mBarAddress = findViewById(R.id.tv_bar_address);
-//        mBarCity = findViewById(R.id.tv_bar_city);
-//        mBarState = findViewById(R.id.tv_bar_state);
-//        mBarCountry = findViewById(R.id.tv_bar_country);
-//        mBarZipCode = findViewById(R.id.tv_bar_zip_code);
 
         Intent intent = getIntent();
         if(intent != null && intent.hasExtra(YelpAPIUtils.EXTRA_SEARCH_RESULT)) {
@@ -45,11 +35,6 @@ public class BarsDetailedActivity extends AppCompatActivity {
             mBarPhoneNumber.setText(yelpItem.phoneNumber);
             mBarLocation.setText(yelpItem.address + ", " + yelpItem.city + ", " + yelpItem.state + "\n" +
             yelpItem.zipCode + ", " + yelpItem.country);
-//            mBarAddress.setText(yelpItem.address);
-//            mBarCity.setText(yelpItem.city);
-//            mBarState.setText(yelpItem.state);
-//            mBarCountry.setText(yelpItem.country);
-//            mBarZipCode.setText(yelpItem.zipCode);
         }
     }
 }
